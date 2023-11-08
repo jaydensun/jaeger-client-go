bf53a9283720623d66231960afc7e9e08427d35e
tandysun <tandysun@tencent.com>
2023-11-08 16:02:41 +0800

    move concurrent http transport to exist file

--- transport/http.go
+++ transport/http.go
@@ -24,7 +24,6 @@ import (
 
 	"github.com/uber/jaeger-client-go/thrift"
 
-	"github.com/uber/jaeger-client-go"
 	j "github.com/uber/jaeger-client-go/thrift-gen/jaeger"
 )
 
@@ -171,4 +170,28 @@ func serializeThrift(obj thrift.TStruct) (*bytes.Buffer, error) {
 		return nil, err
 	}
 	return t.Buffer, nil
+}
+
+type ConcurrentHttpTransport struct {
+	*HTTPTransport
+	pool chan int
+}
+
+func NewConcurrentHttpTransport(httpTransport *HTTPTransport) *ConcurrentHttpTransport {
+	ctp := &ConcurrentHttpTransport{
+		HTTPTransport: httpTransport,
+	}
+	ctp.pool = make(chan int, 100)
+	for i := 0; i < 100; i++ {
+		ctp.pool <- 0
+	}
+	return ctp
+}
+
+func (c *ConcurrentHttpTransport) send(spans []*j.Span) error {
+	<-c.pool
+	defer func() {
+		c.pool <- 0
+	}()
+	return c.HTTPTransport.send(spans)
 }
